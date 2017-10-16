bc4500a3bd6ba57017a520d1889c3f10095ec745
alan <alan@alandeMacBook-Pro.local>
2017-10-16 20:10:42 +0800

    增加属性，根据表结构的字段

--- zebra-common/src/main/java/com/sim/cloud/zebra/model/PackageUser.java
+++ zebra-common/src/main/java/com/sim/cloud/zebra/model/PackageUser.java
@@ -7,43 +7,95 @@ package com.sim.cloud.zebra.model;
 public class PackageUser  extends BaseModel{
 
 	private static final long serialVersionUID = 2371027727547341076L;
-	private Integer uid;
-	private Float cost;
-	private Integer planId;
-	private Integer term;//1月 2月。24月
-	private String remark;//备注
+	private Integer uid;                        // 用户ID
+	private Integer packageType;                // 套餐类型  1-按月套餐、2-加油包、3-超额流量包、4-自定义套餐包
+	private Integer flow;                       // 流量
+	private Integer messages;                   // 短信条数
+	private Integer callMinutes;                // 通话分钟数	
+	private Integer term;                       // 1月 2月。24月  有效期
+	private Float platQuote;                    // 平台价格
+	private Float externalQuote;                // 对外的价格
+	private Integer   operator;                 // 运营商      1-移动、2-电信、3-联通	
+    private Integer planId;                     // 资费计划ID 
+	private String  account;                    // 运营商平台账号	
+	private String  remark;                     // 备注	
 	
-	
-	public String getRemark() {
-		return remark;
-	}
-	public void setRemark(String remark) {
-		this.remark = remark;
-	}
-	public Integer getUid() {
-		return uid;
-	}
-	public void setUid(Integer uid) {
-		this.uid = uid;
-	}
-	public Float getCost() {
-		return cost;
-	}
-	public void setCost(Float cost) {
-		this.cost = cost;
-	}
-	public Integer getPlanId() {
-		return planId;
-	}
-	public void setPlanId(Integer planId) {
-		this.planId = planId;
-	}
-	public Integer getTerm() {
-		return term;
-	}
-	public void setTerm(Integer term) {
-		this.term = term;
-	}
+  public Integer getUid() {
+    return uid;
+  }
+  public void setUid(Integer uid) {
+    this.uid = uid;
+  }
+  public Integer getPackageType() {
+    return packageType;
+  }
+  public void setPackageType(Integer packageType) {
+    this.packageType = packageType;
+  }
+  public Integer getFlow() {
+    return flow;
+  }
+  public void setFlow(Integer flow) {
+    this.flow = flow;
+  }
+  public Integer getMessages() {
+    return messages;
+  }
+  public void setMessages(Integer messages) {
+    this.messages = messages;
+  }
+  public Integer getCallMinutes() {
+    return callMinutes;
+  }
+  public void setCallMinutes(Integer callMinutes) {
+    this.callMinutes = callMinutes;
+  }
+  public Integer getTerm() {
+    return term;
+  }
+  public void setTerm(Integer term) {
+    this.term = term;
+  }
+  public Float getPlatQuote() {
+    return platQuote;
+  }
+  public void setPlatQuote(Float platQuote) {
+    this.platQuote = platQuote;
+  }
+  public Float getExternalQuote() {
+    return externalQuote;
+  }
+  public void setExternalQuote(Float externalQuote) {
+    this.externalQuote = externalQuote;
+  }
+
+  public Integer getPlanId() {
+    return planId;
+  }
+  public void setPlanId(Integer planId) {
+    this.planId = planId;
+  }
+  public String getAccount() {
+    return account;
+  }
+  public void setAccount(String account) {
+    this.account = account;
+  }
+  public String getRemark() {
+    return remark;
+  }
+  public void setRemark(String remark) {
+    this.remark = remark;
+  }
+  public static long getSerialversionuid() {
+    return serialVersionUID;
+  }
+  public Integer getOperator() {
+    return operator;
+  }
+  public void setOperator(Integer operator) {
+    this.operator = operator;
+  }
 	
 	
 }
