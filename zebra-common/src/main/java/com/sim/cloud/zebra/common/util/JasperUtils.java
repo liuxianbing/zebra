1c495a610a5a8df743f407c166cd64f3b002cb05
HenryLv <hongl.lv@haihangyun.com>
2018-01-19 14:30:15 +0800

    update comments

--- zebra-common/src/main/java/com/sim/cloud/zebra/common/util/JasperUtils.java
+++ zebra-common/src/main/java/com/sim/cloud/zebra/common/util/JasperUtils.java
@@ -21,8 +21,8 @@ public class JasperUtils {
 		statusMap.put("ACTIVATION_READY_NAME", 3); // 可激活
 		//statusMap.put("TRIAL_NAME", 3); // ？？？
 		statusMap.put("ACTIVATED_NAME", 4);  // 已激活
-		statusMap.put("DEACTIVATED_NAME", 5); // 已失效
-		statusMap.put("RETIRED_NAME", 6); // 已停用
+		statusMap.put("DEACTIVATED_NAME", 5); // 已停用
+		statusMap.put("RETIRED_NAME", 6); // 已失效
 		//statusMap.put("PURGED_NAME", 7); // ???
 		
 		return statusMap;
@@ -37,8 +37,8 @@ public class JasperUtils {
 		
 		//statusMap.put("STATUS_3", "TRIAL_NAME"); // ？？？
 		statusMap.put("STATUS_4", "ACTIVATED_NAME");  // 已激活
-		statusMap.put("STATUS_5", "DEACTIVATED_NAME"); // 已失效
-		statusMap.put("STATUS_6", "RETIRED_NAME"); // 已停用
+		statusMap.put("STATUS_5", "DEACTIVATED_NAME"); // 已停用
+		statusMap.put("STATUS_6", "RETIRED_NAME"); // 已失效
 		//statusMap.put("STATUS_7", "PURGED_NAME"); // ???
 		
 		return statusMap;
